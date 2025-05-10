21abec5d8041fa7c03575dce5e3c194dddb5650e
Omartkd <buceovenezuela.ca@gmail.com>
2025-05-10 02:14:48 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,29 @@
 module.exports = function(grunt) {
-  grunt.registerTask('default', 'Mi tarea predeterminada', function() {
-    grunt.log.writeln('¡Grunt funciona correctamente!');
+  grunt.initConfig({
+    clean: ['dist/'],
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['public/**'], dest: 'dist/'},
+          {expand: true, src: ['server.js'], dest: 'dist/'}
+        ]
+      }
+    },
+    uglify: {
+      options: {
+        mangle: true
+      },
+      target: {
+        files: {
+          'dist/public/js/app.min.js': ['public/js/**/*.js']
+        }
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('build', ['clean', 'copy', 'uglify']);
 };
